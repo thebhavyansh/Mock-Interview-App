025dbf49cd71e39c3b86b142fffad8f4e5953044
Bhavyansh Tomar <105980945+thebhavyansh@users.noreply.github.com>
2025-03-11 01:18:35 +0530

    Update page.jsx

--- app/(auth)/sign-up/[[...sign-up]]/page.jsx
+++ app/(auth)/sign-up/[[...sign-up]]/page.jsx
@@ -1,15 +1,5 @@
 import { SignUp } from "@clerk/nextjs";
 
 export default function Page() {
-  return
-  <div>
-  <h2 classname="flex justify-center items-center">Sign up with just username and password </h2>
-<section className="bg-white dark:bg-gray-900 mt-10 flex justify-center items-center">
-  
-       <SignUp />
-
-</section>
-</div>
- 
-  
+  return <SignUp />;
 }
