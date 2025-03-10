91b4ddf03742e8b884d796acd946661595d1b988
Bhavyansh Tomar <105980945+thebhavyansh@users.noreply.github.com>
2025-03-11 00:58:02 +0530

    Update page.jsx

--- app/(auth)/sign-in/[[...sign-in]]/page.jsx
+++ app/(auth)/sign-in/[[...sign-in]]/page.jsx
@@ -3,7 +3,7 @@ import { SignIn } from "@clerk/nextjs";
 export default function Page() {
   return (
 
-<section className="bg-white dark:bg-gray-900">
+<section className="bg-white dark:bg-gray-900 flex justify-center items-center">
   
         <SignIn />
 
